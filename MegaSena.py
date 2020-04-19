1f07b254760975d57b838792ae9dc3d183be2539
hudcrash <hrsilvacosta@gmail.com>
2020-04-19 16:55:49 -0300

    first commit

---
+++ MegaSena.py
@@ -0,0 +1,11 @@
+import random
+
+print(
+    'Este são seus números da sorte para ganhar na mega. BOA SORTE !!!'
+    )
+
+for i in range(6):
+
+    valor = random.randint(1, 61)
+
+    print(valor)
